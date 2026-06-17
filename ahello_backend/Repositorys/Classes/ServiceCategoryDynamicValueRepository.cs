f60cfdc198951f2dc0e7bba23b9d32532100f9df
Selvasakthi-e2o <selvasakthi@e2o.in>
2026-06-17 12:31:03 +0530

    servicecategorydynamicvalues

--- ahello_backend/Repositorys/Classes/ServiceCategoryDynamicValueRepository.cs
+++ ahello_backend/Repositorys/Classes/ServiceCategoryDynamicValueRepository.cs
@@ -28,6 +28,22 @@ namespace ahello_backend.Repositorys.Classes
 
             try
             {
+                // DUPLICATE CHECK HERE
+                var duplicateExists = await connection.ExecuteScalarAsync<int>(
+                    @"SELECT COUNT(*)
+                      FROM servicecategorydynamic
+                      WHERE LOWER(ServiceCategoryName) =
+                    LOWER(@ServiceCategoryName)",
+                    new
+                    {
+                        model.ServiceCategoryName
+                    },
+                    tx);
+
+                if (duplicateExists > 0)
+                {
+                    throw new Exception("Service Category Name already exists.");
+                }
                 // Create Category First
                 var serviceCategoryId =
                     await connection.ExecuteScalarAsync<int>(
@@ -286,16 +302,64 @@ namespace ahello_backend.Repositorys.Classes
                 countQuery,
                 parameters);
 
-            var data = await connection.QueryAsync<ServiceCategoryDynamicGetResponse>(
-                dataQuery,
-                parameters);
+            var data = (await connection.QueryAsync<ServiceCategoryDynamicGetResponse>(
+            dataQuery,
+            parameters)).ToList();
+            foreach (var category in data)
+            {
+                var fields =
+                    await connection.QueryAsync<ServiceCategoryDynamicFieldResponse>(
+                    @"SELECT
+                    scf.ServiceCategoryFieldId,
+                    scf.FieldName,
+                    scf.FieldCode,
+                    scf.Placeholder,
+                    scf.IsRequired,
+                    scf.DataTypeId,
+                    scfv.FieldValue
+                    FROM servicecategoryfieldvalues scfv
+                    INNER JOIN servicecategoryfields scf
+                    ON scfv.ServiceCategoryFieldId =
+                       scf.ServiceCategoryFieldId
+                     WHERE scfv.ServiceCategoryId =
+                      @ServiceCategoryId",
+                    new
+                    {
+                        category.ServiceCategoryId
+                    });
 
+                category.Fields = fields.ToList();
+
+                var dropdownOptions =
+                    await connection.QueryAsync<ServiceCategoryDropdownOptionResponse>(
+                    @"SELECT DISTINCT
+                    scdo.ServiceCategoryDropDownId,
+                    scdo.ServiceCategoryFieldId,
+                    scdo.ServiceCategoryId,
+                    scdo.OptionValue,
+                    scdo.OptionLabel,
+                    scdo.IsActive
+                FROM servicecategorydropdownoption scdo
+                INNER JOIN servicecategoryfieldvalues scfv
+                    ON scdo.ServiceCategoryFieldId =
+                       scfv.ServiceCategoryFieldId
+                   AND scdo.ServiceCategoryId =
+                       scfv.ServiceCategoryId
+                WHERE scdo.ServiceCategoryId =
+                      @ServiceCategoryId",
+                    new
+                    {
+                        category.ServiceCategoryId
+                    });
+
+                category.DropdownOptions = dropdownOptions.ToList();
+            }
             return new PagedResult<ServiceCategoryDynamicGetResponse>
             {
                 PageNumber = pageNumber,
                 PageSize = pageSize,
                 TotalCount = totalRecords,
-                Details = data.ToList()
+                Details = data
             };
         }
         public async Task<ServiceCategoryDynamicGetResponse>
@@ -387,7 +451,27 @@ namespace ahello_backend.Repositorys.Classes
             connection.Open();
 
             using var tx = connection.BeginTransaction();
-            await connection.ExecuteAsync(
+            try
+            {
+                // DUPLICATE CHECK
+                var duplicateExists = await connection.ExecuteScalarAsync<int>(
+                    @"SELECT COUNT(*)
+              FROM servicecategorydynamic
+              WHERE LOWER(ServiceCategoryName) =
+                    LOWER(@ServiceCategoryName)
+              AND ServiceCategoryId <> @ServiceCategoryId",
+                    new
+                    {
+                        model.ServiceCategoryName,
+                        ServiceCategoryId = serviceCategoryId
+                    },
+                    tx);
+
+                if (duplicateExists > 0)
+                {
+                    throw new Exception("Service Category Name already exists.");
+                }
+                await connection.ExecuteAsync(
             @"UPDATE servicecategorydynamic
               SET ServiceCategoryName = @ServiceCategoryName,
                  IsActive = @IsActive,
@@ -403,29 +487,29 @@ namespace ahello_backend.Repositorys.Classes
             },
             tx);
 
-            try
-            {
-                await connection.ExecuteAsync(
-                    @"DELETE FROM servicecategoryfieldvalues
+                try
+                {
+                    await connection.ExecuteAsync(
+                        @"DELETE FROM servicecategoryfieldvalues
                       WHERE ServiceCategoryId =
                             @ServiceCategoryId",
-                    new
-                    {
-                        ServiceCategoryId = serviceCategoryId
-                    },
-                    tx);
-                 await connection.ExecuteAsync(
-                @"DELETE FROM servicecategorydropdownoption
+                        new
+                        {
+                            ServiceCategoryId = serviceCategoryId
+                        },
+                        tx);
+                    await connection.ExecuteAsync(
+                   @"DELETE FROM servicecategorydropdownoption
                   WHERE ServiceCategoryId =
                         @ServiceCategoryId",
-                new
-                {
-                    ServiceCategoryId = serviceCategoryId
-                },
-                tx);
-                if (model.Fields != null && model.Fields.Any())
-                {
-                    var sql = @"
+                   new
+                   {
+                       ServiceCategoryId = serviceCategoryId
+                   },
+                   tx);
+                    if (model.Fields != null && model.Fields.Any())
+                    {
+                        var sql = @"
                     INSERT INTO servicecategoryfieldvalues
                     (
                         ServiceCategoryId,
@@ -448,23 +532,23 @@ namespace ahello_backend.Repositorys.Classes
                     WHERE scf.ServiceCategoryFieldId =
                           @ServiceCategoryFieldId";
 
-                    foreach (var field in model.Fields.Where(x => x.ServiceCategoryFieldId > 0))
-                    {
-                        await connection.ExecuteAsync(
-                            sql,
-                            new
-                            {
-                                ServiceCategoryId = serviceCategoryId,
-                                field.ServiceCategoryFieldId,
-                                field.FieldValue,
-                                model.ModifiedBy
-                            },
-                            tx);
+                        foreach (var field in model.Fields.Where(x => x.ServiceCategoryFieldId > 0))
+                        {
+                            await connection.ExecuteAsync(
+                                sql,
+                                new
+                                {
+                                    ServiceCategoryId = serviceCategoryId,
+                                    field.ServiceCategoryFieldId,
+                                    field.FieldValue,
+                                    model.ModifiedBy
+                                },
+                                tx);
+                        }
                     }
-                }
-                if (model.Fields != null && model.Fields.Any())
-                {
-                    var dropdownSql = @"
+                    if (model.Fields != null && model.Fields.Any())
+                    {
+                        var dropdownSql = @"
                     INSERT INTO servicecategorydropdownoption
                     (
                         ServiceCategoryFieldId,
@@ -486,32 +570,38 @@ namespace ahello_backend.Repositorys.Classes
                         @ModifiedBy
                     );";
 
-                    foreach (var field in model.Fields.Where(x => x.ServiceCategoryFieldId > 0))
-                    {
-                        if (field.DropDownOptions != null &&
-                            field.DropDownOptions.Any())
+                        foreach (var field in model.Fields.Where(x => x.ServiceCategoryFieldId > 0))
                         {
-                            foreach (var option in field.DropDownOptions)
+                            if (field.DropDownOptions != null &&
+                                field.DropDownOptions.Any())
                             {
-                                await connection.ExecuteAsync(
-                                    dropdownSql,
-                                    new
-                                    {
-                                        field.ServiceCategoryFieldId,
-                                        ServiceCategoryId = serviceCategoryId,
-                                        option.OptionValue,
-                                        option.OptionLabel,
-                                        option.IsActive,
-                                        model.ModifiedBy
-                                    },
-                                    tx);
+                                foreach (var option in field.DropDownOptions)
+                                {
+                                    await connection.ExecuteAsync(
+                                        dropdownSql,
+                                        new
+                                        {
+                                            field.ServiceCategoryFieldId,
+                                            ServiceCategoryId = serviceCategoryId,
+                                            option.OptionValue,
+                                            option.OptionLabel,
+                                            option.IsActive,
+                                            model.ModifiedBy
+                                        },
+                                        tx);
+                                }
                             }
                         }
                     }
-                }
-                tx.Commit();
+                    tx.Commit();
 
-                return true;
+                    return true;
+                }
+                catch
+                {
+                    tx.Rollback();
+                    throw;
+                }
             }
             catch
             {
