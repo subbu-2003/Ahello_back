03298816d14b7c446fbff334750b6e8fc60bf3bc
Vishal P <vishalvishal9149@outlook.com>
2026-06-18 12:24:18 +0530

    changes in admin login

---
+++ ahello_backend/Models/Admin/AdminCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ahello_backend.Models.Admin
+{
+    public class AdminCreateDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
