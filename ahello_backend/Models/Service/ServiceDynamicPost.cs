172f60088b39974ca18bc702699b80abc8dac6e8
Selvasakthi-e2o <selvasakthi@e2o.in>
2026-07-01 11:48:05 +0530

    servicedynamic error msg add it

--- ahello_backend/Models/Service/ServiceDynamicPost.cs
+++ ahello_backend/Models/Service/ServiceDynamicPost.cs
@@ -1,14 +1,22 @@
-﻿namespace ahello_backend.Models.Service
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ahello_backend.Models.Service
 {
     public class ServiceDynamicPost
     {
-        public int UserId { get; set; }
-        public int ServiceTypeId { get; set; }
+        [Required(ErrorMessage = "User is required.")]
+        public int? UserId { get; set; }
 
-        public int ServiceCategoryId {  get; set; }
+        [Required(ErrorMessage = "Service Type is required.")]
+        public int? ServiceTypeId { get; set; }
+
+        [Required(ErrorMessage = "Service Category is required.")]
+        public int? ServiceCategoryId { get; set; }
 
         public string ServiceTitle { get; set; }
-        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a valid price.")] 
+        public decimal? Price { get; set; }
 
         public string Duration { get; set; }
 
