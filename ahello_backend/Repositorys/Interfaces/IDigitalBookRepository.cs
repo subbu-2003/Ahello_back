4713ffd6d20abffa1cc98ac8d574f40ae10f7b2f
pradeepae2o <pradeepa@e2o.in>
2026-08-21 16:11:12 +0530

    done digitalbook api

---
+++ ahello_backend/Repositorys/Interfaces/IDigitalBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using ahello_backend.Models.DigitalBook;
+
+namespace ahello_backend.Repositorys.Interfaces
+{
+    public interface IDigitalBookRepository
+    {
+        Task<int> CreateAsync(DigitalBook digitalBook);
+
+        Task<bool> UpdateAsync(DigitalBook digitalBook);
+
+        Task<IEnumerable<DigitalBook>> GetAllAsync();
+
+        Task<IEnumerable<DigitalBook>> GetByUserIdAsync(int userId);
+
+        Task<bool> DeactivateAsync(int digitalBookId, int modifiedBy);
+
+        Task<IEnumerable<DigitalBook>> GetBySlugAsync(string slug);
+    }
+}
---
+++ ahello_backend/Services/Classes/DigitalBookService.cs
@@ -0,0 +1,61 @@
+﻿using ahello_backend.Models.DigitalBook;
+using ahello_backend.Repositorys.Interfaces;
+using ahello_backend.Services.Interfaces;
+
+namespace ahello_backend.Services.Classes
+{
+    public class DigitalBookService : IDigitalBookService
+    {
+        private readonly IDigitalBookRepository _repository;
+
+        public DigitalBookService(
+            IDigitalBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // CREATE
+        public async Task<int> CreateAsync(
+            DigitalBook digitalBook)
+        {
+            return await _repository.CreateAsync(digitalBook);
+        }
+
+        // UPDATE
+        public async Task<bool> UpdateAsync(
+            DigitalBook digitalBook)
+        {
+            return await _repository.UpdateAsync(digitalBook);
+        }
+
+        // GET ALL
+        public async Task<IEnumerable<DigitalBook>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        // GET BY USER ID
+        public async Task<IEnumerable<DigitalBook>> GetByUserIdAsync(
+            int userId)
+        {
+            return await _repository.GetByUserIdAsync(userId);
+        }
+
+        // DEACTIVATE
+        public async Task<bool> DeactivateAsync(
+            int digitalBookId,
+            int modifiedBy)
+        {
+            return await _repository.DeactivateAsync(
+                digitalBookId,
+                modifiedBy);
+        }
+
+        // GET BY USER SLUG
+        public async Task<IEnumerable<DigitalBook>> GetBySlugAsync(
+      string slug)
+        {
+            return await _repository.GetBySlugAsync(slug);
+        }
+    }
+}
